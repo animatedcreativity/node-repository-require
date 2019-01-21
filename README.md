dbbf08c43ac532c614d7ecf69adb37042f9e1e49
Glitch (node-repository) <none>
2019-01-21 06:37:11 +0000

    🕋⛩ Updated with Glitch

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
 # node-repository
 Install a Node.js module from any public/private repository easily.
+
+---------------------------------------------------
+
+**NOTE:** If you include public/private git links in package.json dependencies, they will also work by default without this module, and will be installed by `npm update`.
+
+---------------------------------------------------
+
+Why the module was made?<br/>
+**Made this module just because Glitch.com (https://glitch.com/) was not supporting installing my private Github repositories, at this time.**
+
+---------------------------------------------------
+
+**ToDo:**
+- Install dependencies automatically.
+- Remove dependencies automatically from package.json just like they are added.
+
+---------------------------------------------------
+
+**Usage:**
+
+Create installer with optional config, it will use defaults if not provided.
+```
+var nodeRepository = require("node-repository");
+var installer = new nodeRepository({consoleLog: true});
+```
+
+Require module
+```
+var result = installer.require("module-name", "https://<github_token>:x-oauth-basic@github.com/<user>/<module_name>.git");
+```
+
+Run npm update in console just one time once you have required a new module from repository. This is because once the module is downloaded, its dependencies are added in package.json
+```
+npm update
+```
+
+Check if the module was installed
+```
+var installer = new exports();
+installer.require("module-name", "https://<github_token>:x-oauth-basic@github.com/<user>/<module_name>.git")
+  .then(function(result) {
+    var module = require("module-name");
+    // do something with module
+  });
+```
+
+---------------------------------------------------
+
+**Repository links:**
+
+- **public**: A normal repository link like: https://github.com/<user>/<module_name>.git
+
+- **private**: Repository link with access token for example for Github, like: https://<github_token>:x-oauth-basic@github.com/<user>/<module_name>.git
+
+---------------------------------------------------
+
+**`installer` Methods**
+
+Checks if module already exists, installs/reinstalls if required.
+
+`.require(name, link, forceReinstall)`
+- name: module name to use
+- link: module's public/private repository link
+- forceReinstall: Force reinstall a module, useful if there is a new version for the module available from the repository.
+
+`.exists(name)`
+
+Checks if module already exists.
+
+- name: module name to use
+
+`.path(name)`
+
+Gets absolute module path.
+
+- name: module name to use
+
+`.version(name)`
+
+Gets installed module's version.
+
+- name: module name to use
+
+`.install(name, link)`
+
+Checks if module already exists, installs if required. Does not reinstall.
+
+- name: module name to use
+- link: module's public/private repository link
+
+`.addDependencies(name)`
+
+Finds dependencies in a module's package.json and adds them to current project's package.json. `npm update` is necessary after this.
+
+- name: module name to use
---
+++ main.js
@@ -0,0 +1,95 @@
+module.exports = exports = function(config) {
+  var sanitize = require("node-sanitize-options");
+  config = sanitize.options(config, {
+    consoleLog: true
+  });
+  var fs = require("fs");
+  var rimraf = require("rimraf");
+  var fileJson = require("node-file-json");
+  var installer = {
+    require: function(name, link, forceReinstall) {
+      return new Promise(function(resolve, reject) {
+        var result = "";
+        if (!installer.exists(name) || forceReinstall === true) {
+          var path = installer.path(name);
+          rimraf.sync(path);
+          installer.install(name, link).then(function(message) {
+            resolve(message);
+          }).catch(function(error) {
+            reject(error);
+          });
+        } else {
+          installer.addDependencies(name);
+          if (config.consoleLog === true) console.log("node-repository: module: " + name + "@" + installer.version(name) + " already installed.");
+          resolve({status: true, message: "Already exists"});
+        }
+      });
+    },
+    exists: function(name) {
+      var path = installer.path(name);
+      if (fs.existsSync(path) && fs.existsSync(path + "/package.json")) {
+        return true;
+      }
+      return false;
+    },
+    path: function(name) {
+      return __dirname + "/node_modules/" + name;
+    },
+    version: function(name) {
+      var version = "";
+      if (installer.exists(name) === true) {
+        var path = installer.path(name);
+        var json = new fileJson();
+        json.load(path + "/package.json");
+        version = json.data.version;
+      }
+      return version;
+    },
+    install: function(name, link) {
+      return new Promise(function(resolve, reject) {
+        var path = installer.path(name);
+        if (!fs.existsSync(path)) {
+          var git = require("nodegit");
+          git.Clone(link, path).then(function(repository) {
+            installer.addDependencies(name);
+            if (config.consoleLog === true) console.log("node-repository: module: " + name + "@" + installer.version(name) + " installed, do not forget to run (npm update) in project root");
+            resolve({status: true, message: "Installed", repository: repository});
+          }).catch(function(error) {
+            reject(error);
+          });
+        } else {
+          installer.addDependencies(name);
+          if (config.consoleLog === true) console.log("node-repository: module: " + name + "@" + installer.version(name) + " already installed.");
+          resolve({status: true, message: "Already exists"});
+        }
+      });
+    },
+    addDependencies: function(name) {
+      if (installer.exists(name)) {
+        var path = installer.path(name);
+        var repoJson = new fileJson();
+        repoJson.load(path + "/package.json");
+        var json = new fileJson();
+        json.load("./package.json");
+        var changed = false;
+        for (var key in repoJson.data.dependencies) {
+          var module = key + "@" + repoJson.data.dependencies[key];
+          if (json.data[key] != repoJson.data[key]) {
+            if (config.consoleLog === true) console.log("node-repository: adding dependency: " + module);
+            json.data.dependencies[key] = repoJson.data.dependencies[key];
+            changed = true;
+          }
+        }
+        if (changed === true) {
+          json.save();
+          if (config.consoleLog === true) {
+            console.log("node-repository: please check a copy of OLD package.json below, if needed.");
+            console.log(json.data);
+            console.log("node-repository: run (npm update) in project root");
+          }
+        }
+      }
+    }
+  }
+  return installer;
+};
